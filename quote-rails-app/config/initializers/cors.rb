d508c9716baf792114612d6160f6dbd26b3da7e9
Aditi Green <asaki444@gmail.com>
2017-12-12 01:22:46 -0500

    added api routes

--- quote-rails-app/config/initializers/cors.rb
+++ quote-rails-app/config/initializers/cors.rb
@@ -9,8 +9,12 @@
 #   allow do
 #     origins 'example.com'
 #
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'localhost:3000'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
--- quote-rails-app/config/routes.rb
+++ quote-rails-app/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+  namespace :api do
+    resources :quotes, except: [:new, :edit]
+    resources :authors, only: [:show, :index]
+  end
+end
 end
