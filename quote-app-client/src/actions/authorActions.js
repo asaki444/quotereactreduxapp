5ef6a58fe9f680b3981386f9e1fbfbcc92eea16b
Aditi Green <asaki444@gmail.com>
2017-12-14 00:38:10 -0500

    installed isomorphic fetch, and adjusted the post request

--- quote-app-client/src/actions/authorActions.js
+++ quote-app-client/src/actions/authorActions.js
@@ -2,6 +2,7 @@ import fetch from 'isomorphic-fetch'
 const API_URL = process.env.REACT_APP_API_URL
 export function fetchAuthors(){
   return (dispatch)=>{
+    debugger
     dispatch({type: 'LOADING_QUOTES'})
     return fetch('${API_URL}/authors')
       .then(res => res.json())
--- quote-app-client/src/actions/quoteActions.js
+++ quote-app-client/src/actions/quoteActions.js
@@ -3,20 +3,25 @@ const API_URL = process.env.REACT_APP_API_URL;
 export function fetchQuotes(){
   return (dispatch)=>{
     dispatch({type: 'LOADING_QUOTES'})
-    return fetch('${API_URL}/quotes')
+    return fetch(`${API_URL}/quotes`)
       .then(res => res.json())
       .then(json => dispatch({type: 'FETCH_QUOTES', payload: json}))
   }
 }
 
 export function addQuote(quote){
-  return (dispatch)=>{
-    dispatch({type: 'ADD_QUOTE', quote})
-      return fetch('http:/localhost:3001/api/quotes',
-       {method: 'POST',
-        body: JSON.stringify(quote),
-        contentType: 'application/json',
-        accepts: 'application/json'}
-    ).then(response=> response.json())
+  return dispatch => {
+    return fetch(`${API_URL}/quotes`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ quote: quote })
+    })
+      .then(response => response.json())
+      .then(quote => {
+        dispatch({type: "ADD_QUOTE", quote})
+      })
+      .catch(error => console.log(error))
    }
-   }
+ }
