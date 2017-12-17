aadee03906402b60e39b02a47c92cfc93c6705b1
Aditi Green <asaki444@gmail.com>
2017-12-17 16:30:41 -0500

    added some styling to the quotes index page, and author index page

--- quote-app-client/src/components/Author.js
+++ quote-app-client/src/components/Author.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 
 const Author = (props)=>{
   return(
-     <div className='author'>
+     <div className='Author'>
       <h2>{props.author.name}</h2>
-      <h3>{props.author.quotes}</h3>
     </div>
 
   )
