791073b158e5be12859a73cbd00a2093dbb41103
Aditi Green <aditigreen@Aditis-MacBook-Pro.local>
2018-10-09 20:57:16 -0400

    added two buttons to the log in page

--- quote-app-client/src/components/LogInPage.js
+++ quote-app-client/src/components/LogInPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import SignIn from './containers/SignInPage';
+import LogIn from './containers/LogInPage';
 
 const LogInPage = ()=>{
-  
+    let SignIn = <SignIn/>
+    let LogIn = <SignUp/>
     return(
         <div className="LogInHeader">
           <h1>Log in Or Sign Up to Begin</h1>
-
+          <button>Log In </button>
+          <button> Sign Up </button>
+            <div id="access">
+            </div>
         </div>
     )
 }
