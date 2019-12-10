33e29e21ca9e55041e4f894e9273f700ed125045
Aditi <asaki444@gmail.com>
2019-12-09 23:38:21 -0500

    Update user.rb

--- quote-rails-app/app/models/user.rb
+++ quote-rails-app/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :quotes
+  has_many :authors, through: :quotes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
